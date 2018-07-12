9a54ae070b04a6d5640497abf9a64cd9ebba2f30
soul4rent <kyle.a.wadsworth@gmail.com>
2018-07-12 11:19:49 -0700

    create and read done

--- index.js
+++ index.js
@@ -2,17 +2,49 @@ const { GraphQLServer } = require('graphql-yoga')
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
+
+
+const Todo = mongoose.model('Todo', {
+  text: String,
+  complete: Boolean
+});
+
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos: [Todo]
+  }
+  type Todo{
+    id: ID!
+    text: String!
+    complete: Boolean!
+  }
+  type Mutation{
+    createTodo(text: String!): Todo
+
   }
 `
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
+    todos: () => Todo.find()
   },
-}
+  Mutation: {
+    createTodo: async (_, { text }) => {
+      const todo = new Todo({ text, complete: false });
+      await todo.save();
+      return todo;
+    }
+  }
+};
 
 const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+
+mongoose.connection.once('open', function() {
+
+  server.start(() => console.log('Server is running on localhost:4000'));
+
+});
